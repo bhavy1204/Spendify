b5ae6c4debb6eac889efd416f8e3802ef042a8de
bhavy1204 <bhavysarwa51@gmail.com>
2025-12-16 09:08:53 +0530

    feat: add PWA support

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,44 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss()],
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.svg"],
+      manifest: {
+        name: "Spendify",
+        short_name: "Spendify",
+        description: "Simple offline home finance tracker",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#f8fafc",
+        theme_color: "#0f172a",
+        icons: [
+          {
+            src: "/pwa-192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "/pwa-512.png",
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ]
+      }
+    })
+  ],
 })
+
+
+
+
+
+
+
